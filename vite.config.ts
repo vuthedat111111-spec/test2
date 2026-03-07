d60718aa3da305f6559880afea0eeea7b7ec0dbb
vuthedat111111-spec <vuthedat111111@gmail.com>
2026-03-07 17:22:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
+ 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
