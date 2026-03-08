af88cd3b40a2a391e165cf31911ee9fcec36826b
vuthedat111111-spec <vuthedat111111@gmail.com>
2026-03-08 11:43:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1037,7 +1037,7 @@ else if (dbData?.ALPHABETS?.katakana?.[char]) info = dbData.ALPHABETS.katakana[c
 return (
     <div 
         
-        className="fixed inset-0 z-[200] flex items-center justify-center bg-black/60 backdrop-blur-sm animate-in fade-in duration-200 cursor-pointer"
+        className="fixed inset-0 z-[1000] flex items-center justify-center bg-black/60 backdrop-blur-sm animate-in fade-in duration-200 cursor-pointer"
         onClick={onClose} 
     >
         <div 
