058e8930f0718fa6879968d22f33795f83913da1
vuthedat111111-spec <vuthedat111111@gmail.com>
2026-03-21 18:34:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const calculateSRS = (currentData, quality) => {
   if (nextReview && nextReview > now) {
       if (quality === 1) return currentData;
   }
- 
+  
   if (quality === 0) {
     easeFactor = Math.max(1.3, easeFactor - 0.2);
     
