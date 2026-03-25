6ecc992924999c6c85a27b29209f270456391d67
vuthedat111111-spec <vuthedat111111@gmail.com>
2026-03-25 21:56:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5056,7 +5056,10 @@ const KaiwaModal = ({ isOpen, onClose }) => {
         '22baitrungthuongcap': [
             { title: "Phần 1", desc: "Hội thoại với gia đình, người yêu", lessonCount: 5 },
             { title: "Phần 2", desc: "Hội thoại với bạn bè", lessonCount: 5 },
-            { title: "Phần 3", desc: "Hội thoại với người quen, hàng xóm", lessonCount: 5 }
+            { title: "Phần 3", desc: "Hội thoại với người quen, hàng xóm", lessonCount: 5 },
+            { title: "Phần 4", desc: "Hội thoại với bác sĩ, nhân viên cửa hàng...", lessonCount: 5 },
+            { title: "Phần 5", desc: "Hội thoại với đồng nghiệp", lessonCount: 5 },
+            { title: "Phần 6", desc: "Hội thoại giữa cấp trên, cấp dưới", lessonCount: 5 }
         ]
     };
 
