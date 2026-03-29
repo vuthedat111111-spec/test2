e3455fbd32e31230148f0dcb1afede235c786e71
vuthedat111111-spec <vuthedat111111@gmail.com>
2026-03-30 00:07:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7254,7 +7254,7 @@ const DictationPracticeView = ({ lessonData, onBack, onClose }) => {
                             </button>
                             <span className="text-[9px] sm:text-[10px] text-zinc-400 font-bold uppercase tracking-widest flex items-center gap-1">
                                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><polyline points="9 10 4 15 9 20"></polyline><path d="M20 4v7a4 4 0 0 1-4 4H4"></path></svg>
-                                Enter
+                                Bấm Enter để kiểm tra
                             </span>
                         </div>
                     </div>
